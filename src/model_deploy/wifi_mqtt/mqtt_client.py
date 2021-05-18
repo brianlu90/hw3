60b9523b5e3cd7df2d7917c5c8da30d4a29d1b1a
brianlu90 <brianlu90@gmail.com>
2021-05-18 02:53:35 -0700

    add python code

--- src/model_deploy/wifi_mqtt/mqtt_client.py
+++ src/model_deploy/wifi_mqtt/mqtt_client.py
@@ -14,23 +14,29 @@ s = serial.Serial(serdev, 9600)
 host = "192.168.43.162"
 topic1= "Gesture"
 topic2= "Angle"
-
+i = 0;
 # Callbacks
 def on_connect(self, mosq, obj, rc):
     print("Connected rc: " + str(rc))
 
 def on_message(mosq, obj, msg):
-    print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n");
+    global i
     if msg.topic  == topic1:
+        print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n");
         s.write("/GestureUI_End/run\r\n".encode())
         print("Start measuring tilt angle...")
         time.sleep(0.5)
         s.write("/AngleMsr/run\r\n".encode())
     elif msg.topic  == topic2:
-        s.write("/AngleMsr_End/run\r\n".encode())
-        print("Start detecting gesture...")
-        time.sleep(0.5)
-        s.write("/GestureUI/run\r\n".encode())
+        if i >= 3:
+            s.write("/AngleMsr_End/run\r\n".encode())
+            print("Start detecting gesture...")
+            time.sleep(0.5)
+            s.write("/GestureUI/run\r\n".encode())
+            i = 0
+        else:
+            print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n");
+            i = i + 1
 
 def on_subscribe(mosq, obj, mid, granted_qos):
     print("Subscribed OK")
